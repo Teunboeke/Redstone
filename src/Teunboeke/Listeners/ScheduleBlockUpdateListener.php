6963f44f4905a006a01b815be978fad7512d4ea6
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-12-26 14:48:45 +0100

    Create ScheduleBlockUpdateListener.php

---
+++ src/Teunboeke/Listeners/ScheduleBlockUpdateListener.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Teunboeke\listeners;
+
+use pocketmine\event\Listener;
+
+use pocketmine\event\level\LevelLoadEvent;
+use pocketmine\event\level\LevelSaveEvent;
+
+use Teunboeke\Main;
+
+class ScheduledBlockUpdateListener implements Listener {
+
+    public function onLevelLoad(LevelLoadEvent $event) : void {
+        Main::getInstance()->getScheduledBlockUpdateLoader()->loadLevel($event->getLevel());
+    }
+
+    public function onLevelSave(LevelSaveEvent $event) : void {
+        Main::getInstance()->getScheduledBlockUpdateLoader()->saveLevel($event->getLevel());
+    }
+}
