bc282443a2be0e9299a4f2075d6c50d77fe9d5e0
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-12-26 22:57:03 +0100

    Update Dirt.php

--- src/Teunboeke/blocks/Dirt.php
+++ src/Teunboeke/blocks/Dirt.php
@@ -5,3 +5,10 @@ namespace Teunboeke Teunboeke\blocks;
 use pocketmine\block\Block;
 use pocketmine\block\BlockFactory;
 use pocketmine\item\Hoe;
+use pocketmine\item\Item;
+use pocketmine\Player;
+
+class Dirt extends \pocketmine\block\Dirt
+  
+      public function onActivate(Item $item, Player $player = null) : bool{
+          if($item instanceof Hoe or $item instanceof \Teunboeke\){
