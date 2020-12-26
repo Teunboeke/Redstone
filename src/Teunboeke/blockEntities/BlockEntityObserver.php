369fd043d59f3d6477cb0018f946519d42c0cc18
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-12-26 12:17:48 +0100

    Create BlockEntityObserver.php

---
+++ src/Teunboeke/blockEntities/BlockEntityObserver.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Teunboeke\blockEntities;
+
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\tile\Tile;
+
+class BlockEntityObserver extends Tile {
+
+    private $runtimeId;
+    
+    protected function readSaveData(CompoundTag $nbt) : void {
+        if ($nbt->hasTag("runtimeId")) {
+            $this->runtimeId = $nbt->getInt("runtimeId");
+        }
+    }
+
+    protected function writeSaveData(CompoundTag $nbt) : void {
+        $nbt->setInt("runtimeId", $this->runtimeId);
+    }
+
+    public function getSideRuntimeId() : ?int {
+        return $this->runtimeId;
+    }
+
+    public function setSideRuntimeId(int $runtimeId) : void {
+        $this->runtimeId = $runtimeId;
+    }
