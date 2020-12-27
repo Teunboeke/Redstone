40d01e653535ef7792272a1510bc2727e32dad52
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-12-27 10:49:54 +0100

    Update Dirt.php

--- src/Teunboeke/blocks/Dirt.php
+++ src/Teunboeke/blocks/Dirt.php
@@ -12,3 +12,17 @@ class Dirt extends \pocketmine\block\Dirt
   
       public function onActivate(Item $item, Player $player = null) : bool{
           if($item instanceof Hoe or $item instanceof \Teunboeke\){
+            $item->applyDamage(1);
+            if($this->meta === 1){      
+               $this->getLevel()->setBlock($this, BlockFactory::get(Block::DIRT), true);
+            }else{       
+               $this->getLevel()->setBlock($this, BlockFactory::get(Block::FARMLAND), true);
+            }        
+            
+            return true;
+          }      
+  
+          return false;
+      }
+
+  }
